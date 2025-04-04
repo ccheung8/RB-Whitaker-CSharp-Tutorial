33a5c4e42ae390014fbef9d1e7aa2c64d62c64b6
ccheung8 <calvincheung881@gmail.com>
2025-04-03 20:30:12 -0700

    added and completed enumerations section

---
+++ Enumerations/Program.cs
@@ -0,0 +1,11 @@
+﻿// enums are accessed like this
+DayOfWeek today = DayOfWeek.Thursday;
+
+// casting enums
+int dayAsInt = (int)DayOfWeek.Sunday;
+DayOfWeek tomorrow = (DayOfWeek)(dayAsInt + 1);
+Console.WriteLine(tomorrow.ToString());
+
+// enums are declared at the bottom of document
+public enum DayOfWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+// enums are numbered by default similar to arrays but they can be assigned numbers

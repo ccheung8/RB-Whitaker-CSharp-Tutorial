5c721d450508dc71492420f4ff45444b7fe4d6f9
ccheung8 <calvincheung881@gmail.com>
2025-04-07 15:14:10 -0700

    completed and added delegates section

---
+++ Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+// pass in the name of the method with parenthesis
+ModifyArray(numbers, AddOne);
+ModifyArray(numbers, Double);
+ModifyArray(numbers, TripleAndAddOne);
+
+static int AddOne(int input) { return input + 1; }
+static int Double(int input) { return input * 2; }
+static int TripleAndAddOne(int input) { return input * 3 + 1; }
+
+// the goal is to combine all of the methods above into one
+static void ModifyArray(int[] numbers, NumberDelegate desiredChange) {
+  for (int i = 0; i < numbers.Length; i++) {
+    numbers[i] = desiredChange(numbers[i]);
+  }
+}
+
+// delegates go on the bottom of the file along with enumeration, class, struct
+// and interface definitions
+// the int input parameter comes from a common theme among the other methods
+public delegate int NumberDelegate(int input);

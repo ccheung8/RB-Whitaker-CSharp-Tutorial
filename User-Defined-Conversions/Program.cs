49985698bb13a8f8da4b4110eebcf029ec3c231f
ccheung8 <calvincheung881@gmail.com>
2025-04-08 03:04:58 -0700

    completed and added user-defined conversions section

---
+++ User-Defined-Conversions/Program.cs
@@ -0,0 +1,28 @@
+﻿// this works thanks to our implicit conversion
+int aNumber = 3;
+MagicNumber magicNumber = aNumber;
+Console.WriteLine("MagicNumber object: " + magicNumber.Number + ", " + magicNumber.IsMagic);
+
+// this works thanks to our explicit conversion
+MagicNumber magicNumber1 = new MagicNumber() { Number = 3, IsMagic = true };
+int aNumber1 = (int)magicNumber1;
+Console.WriteLine(aNumber1);
+
+public class MagicNumber {
+  public int Number {  get; set; }
+  public bool IsMagic {  get; set; }
+
+  // converts an int to a MagicNumber
+  // must be marked public and static
+  // we specify implicit or explicit
+  // implicit means it convers automatically on assignment
+  // public static [implicit/explicit] operator [type that's being converted](what's being converted from)
+  public static implicit operator MagicNumber(int value) {
+    return new MagicNumber() { Number = value, IsMagic = false };
+  }
+
+  // explicit cast from MagicNumber to int
+  public static explicit operator int(MagicNumber magicNumber) {
+    return magicNumber.Number;
+  }
+}

9b922876371367a4bf67d5c2a5123a22fe877aa8
ccheung8 <calvincheung881@gmail.com>
2025-04-08 03:35:10 -0700

    completed and added extension methods section

---
+++ Extension-Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Sun's getting real low.".ToRandomCase());
+
+// may be good practice to name the class [object type] + Extensions
+// needs to be static
+public static class StringExtensions {
+  private static Random random = new Random();
+  
+  // first param must be type of object we're creating the extension
+  // method for
+  public static string ToRandomCase(this string text) {
+    string result = "";
+
+    for (int i = 0; i < text.Length; i++) {
+      if (random.Next(2) == 0) {
+        result += text.Substring(i, 1).ToUpper();
+      } else {
+        result += text.Substring(i, 1).ToLower();
+      }
+    }
+
+    return result;
+  }
+}
